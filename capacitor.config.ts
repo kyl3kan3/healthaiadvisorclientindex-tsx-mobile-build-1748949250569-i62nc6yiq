f917248dc7df4e2ea0800d73a38d4e6bc46a0da0
kyl3kan3 <kyl3kan3@gmail.com>
2025-06-03 06:14:15 -0500

    Add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aiappbuilder.healthaiadvisorclientindextsxmobilebuild1748949250569i62nc6yiq',
+  appName: 'HealthAiAdvisorclientindex.tsx',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
